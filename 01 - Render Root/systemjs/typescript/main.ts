3575a8811a0da3f0652edaae6e9a2feaa71397bc
Rick Beerendonk <rick@beerendonk.com>
2018-02-24 22:30:42 +0100

    Add Render Root examples for JavaScript and TypeScript.

---
+++ 01 - Render Root/systemjs/typescript/main.ts
@@ -0,0 +1,11 @@
+/*! Mozilla Public License Version 2.0 !*/
+/*! Copyright © 2018 Rick Beerendonk   !*/
+
+import Vue from 'vue';
+
+new Vue({
+  el: '#app',
+  data: {
+    greeting: 'Hello World!'
+  }
+});
---
+++ 01 - Render Root/systemjs/typescript/systemjs.config.js
@@ -0,0 +1,43 @@
+/*! Mozilla Public License Version 2.0 !*/
+/*! Copyright © 2018 Rick Beerendonk   !*/
+
+/* global SystemJS */
+
+SystemJS.config({
+  meta: {
+    '*.js': {
+      babelOptions: {
+        es2015: true
+      }
+    }
+  },
+  paths: {
+    // paths serve as alias
+    'npm:': '../../../node_modules/'
+  },
+  map: {
+    'ts': 'npm:plugin-typescript',
+    'typescript': 'npm:typescript',
+    'vue': 'npm:vue/dist/vue.js'
+  },
+  packages: {
+    '.': {
+      defaultExtension: 'ts'
+    },
+    'ts': {
+      'main': 'lib/plugin.js'
+    },
+    'typescript': {
+      'main': 'lib/typescript.js',
+      'meta': {
+        'lib/typescript.js': {
+          'exports': 'ts'
+        }
+      }
+    }
+  },
+  transpiler: 'ts',
+  typescriptOptions: {
+    tsconfig: true
+  }
+});
