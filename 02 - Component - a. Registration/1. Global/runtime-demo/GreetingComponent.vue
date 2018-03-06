bee1fac628d115feb18db0d89fad3c7dbffd1692
Rick Beerendonk <rick@beerendonk.com>
2018-03-06 06:18:14 -0500

    Add in browser .vue file compilation for presentations.

---
+++ 02 - Component - a. Registration/1. Global/runtime-demo/GreetingComponent.vue
@@ -0,0 +1,16 @@
+<!-- Mozilla Public License Version 2.0 -->
+<!-- Copyright © 2018 Rick Beerendonk   -->
+
+<template>
+  <h1>Hello World!</h1>
+</template>
+
+<script>
+// @vue/component
+module.exports = {
+  name: 'GreetingComponent'
+};
+</script>
+
+<style scoped>
+</style>
