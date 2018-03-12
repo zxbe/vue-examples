48737c7d31ff8811d032bc737dee4478b00048b1
Rick Beerendonk <rick@beerendonk.com>
2018-03-12 16:45:43 -0500

    Add Data Binding - View to Data - Events changing Data example.

---
+++ 03 - Data Binding/2. View to Data/a. Events changing Data/runtime/main.js
@@ -0,0 +1,11 @@
+/*! Mozilla Public License Version 2.0 !*/
+/*! Copyright © 2018 Rick Beerendonk   !*/
+
+import Vue from '../../../../node_modules/vue/dist/vue.esm.browser.js';
+
+new Vue({
+  el: '#app',
+  data: {
+    counter: 0
+  }
+});
