57f17ac364cf82933810fe95a0c02b959ede17c9
Rick Beerendonk <rick@beerendonk.com>
2018-03-12 16:45:59 -0500

    Add Data Binding - View to Data - Events calling Methods example.

---
+++ 03 - Data Binding/2. View to Data/b. Events calling Methods/runtime/main.js
@@ -0,0 +1,15 @@
+/*! Mozilla Public License Version 2.0 !*/
+/*! Copyright © 2018 Rick Beerendonk   !*/
+
+import Vue from '../../../../node_modules/vue/dist/vue.esm.browser.js';
+
+new Vue({
+  el: '#app',
+  methods: {
+    clicked: function (e) {
+      alert(`Event handled.
+      Target: ${e.target}
+      Value: ${e.target.value}`);
+    }
+  }
+});
