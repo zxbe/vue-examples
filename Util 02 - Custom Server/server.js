730cb460ddb091d2d7271a7a4454db79ae2d472c
Rick Beerendonk <rick@beerendonk.com>
2018-03-30 11:17:18 -0500

    Display message (feedback) when there is no path to open.

--- Util 02 - Custom Server/server.js
+++ Util 02 - Custom Server/server.js
@@ -17,35 +17,38 @@ const basePath = process.cwd();
 //console.log('filePath: ' + filePath);
 //console.log('basePath: ' + basePath);
 
-// If a filePath is given, open a browser at this location.
-if (filePath.startsWith(basePath)) {
-  if (fs.statSync(filePath).isDirectory()) {
-    filePath += '/';
-
-    const webpackConfigPath = filePath + 'webpack.config.js';
-    if (fs.existsSync(webpackConfigPath)) {
-      // Webpack config exists, so execute webpack
-      console.log('webpack');
-      //childProcess.spawnSync('webpack', ['.', '--config', webpackConfigPath]);
-      childProcess.spawnSync('webpack-dev-server', ['.', '--hot', '--open', '--config', webpackConfigPath, '--content-base', filePath + 'site/dist']);
-      console.log('webpack done');
-    }
-  }
-
-  const serverUri = `http://localhost:${portHttp}`;
-  const extraPath = filePath.substring(basePath.length);
-  const extraUri = `${serverUri}${extraPath}`;
-
-  //console.log('serverUri: ' + serverUri);
-  //console.log('extraPath: ' + extraPath);
-  //console.log('extraUri: ' + extraUri);
-
-  // See: https://github.com/indexzero/http-server/
-
-  const httpChild = childProcess.spawn('http-server', ['.', '-p', portHttp, '-c-1']);
-
-  const command = process.platform === "win32" ? `start "" "${extraUri}"` :
-                /* process.platform === "darwin" */ `open "${extraUri}"`;
-
-  childProcess.exec(command);
+// Exit if no filePath is given
+if (!filePath || !filePath.startsWith(basePath)) {
+  console.warn('No file path to open.');
+  process.exit();
 }
+
+if (fs.statSync(filePath).isDirectory()) {
+  filePath += '/';
+
+  const webpackConfigPath = filePath + 'webpack.config.js';
+  if (fs.existsSync(webpackConfigPath)) {
+    // Webpack config exists, so execute webpack
+    console.log('webpack');
+    //childProcess.spawnSync('webpack', ['.', '--config', webpackConfigPath]);
+    childProcess.spawnSync('webpack-dev-server', ['.', '--hot', '--open', '--config', webpackConfigPath, '--content-base', filePath + 'site/dist']);
+    console.log('webpack done');
+  }
+}
+
+const serverUri = `http://localhost:${portHttp}`;
+const extraPath = filePath.substring(basePath.length);
+const extraUri = `${serverUri}${extraPath}`;
+
+//console.log('serverUri: ' + serverUri);
+//console.log('extraPath: ' + extraPath);
+//console.log('extraUri: ' + extraUri);
+
+// Start server
+// See: https://github.com/indexzero/http-server/
+const httpChild = childProcess.spawn('http-server', ['.', '-p', portHttp, '-c-1']);
+
+// Open browser
+const command = process.platform === "win32" ? `start "" "${extraUri}"` :
+              /* process.platform === "darwin" */ `open "${extraUri}"`;
+childProcess.exec(command);
