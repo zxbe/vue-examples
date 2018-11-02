33f38ec1dda700e54ac340932562b48ccf792699
Rick Beerendonk <rick@beerendonk.com>
2018-11-02 17:11:41 +0100

    Update versions.

--- 06 - Component - b. Properties/2. Local/runtime/GreetingComponent.js
+++ 06 - Component - b. Properties/2. Local/runtime/GreetingComponent.js
@@ -1,6 +1,8 @@
 /*! Mozilla Public License Version 2.0 !*/
 /*! Copyright © 2018 Rick Beerendonk   !*/
 
+/* eslint vue/require-prop-types: "off" */
+
 // @vue/component
 export default {
   name: 'GreetingComponent',
