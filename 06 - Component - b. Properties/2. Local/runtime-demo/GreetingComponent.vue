11117788fb0a1fae83a923de24bd7cae038a260f
Rick Beerendonk <rick@beerendonk.com>
2019-02-28 22:02:59 +0100

    Align.

--- 06 - Component - b. Properties/2. Local/runtime-demo/GreetingComponent.vue
+++ 06 - Component - b. Properties/2. Local/runtime-demo/GreetingComponent.vue
@@ -1,5 +1,5 @@
 <!-- European Union Public License version 1.2 -->
-<!-- Copyright © 2018 Rick Beerendonk   -->
+<!-- Copyright © 2018 Rick Beerendonk          -->
 
 <template>
   <h1>Hello {{ name }}!</h1>
