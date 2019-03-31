b0d1c2333faf1017a7532f6e43484dee25298489
Rick Beerendonk <rick@beerendonk.com>
2019-03-31 08:01:40 -0500

    Don't use different component registrations everywhere.

--- 06 - Component - b. Properties - i. Types/runtime/main.js
+++ 06 - Component - b. Properties - i. Types/runtime/main.js
@@ -1,14 +1,15 @@
 /*! European Union Public License version 1.2 !*/
 /*! Copyright © 2018 Rick Beerendonk          !*/
 
-import Vue from '../../../node_modules/vue/dist/vue.esm.browser.js';
+import Vue from '../../node_modules/vue/dist/vue.esm.browser.js';
 
 import GreetingComponent from './GreetingComponent.js';
 
-// Register component globally
-Vue.component('greeting-component', GreetingComponent);
-
 // Bootstrap
 new Vue({
-  el: '#app'
+  el: '#app',
+  // Register component locally
+  components: {
+    'greeting-component': GreetingComponent
+  }
 });
