ee70e96858f3ff90e473ff5affae3a7ffd04c1a6
Rick Beerendonk <rick@beerendonk.com>
2019-04-03 10:47:33 -0500

    Add "Vuex - Mutations - With Payload" example.

---
+++ Vuex - 02 - Mutations - a. With Payload/runtime/main.js
@@ -0,0 +1,27 @@
+/*! European Union Public License version 1.2 !*/
+/*! Copyright © 2019 Rick Beerendonk          !*/
+
+import Vue from '../../node_modules/vue/dist/vue.esm.browser.js';
+import Vuex from '../../node_modules/vuex/dist/vuex.esm.js';
+
+import { addToList } from './helpers.js';
+
+// Always call this first.
+Vue.use(Vuex);
+
+const store = new Vuex.Store({
+  state: {
+    name: 'CreateStore'
+  },
+  mutations: {
+    changeName(state, payload) {
+      state.name = payload;
+    }
+  }
+});
+
+addToList(store.state);
+
+store.commit('changeName', 'ChangedName');
+
+addToList(store.state);
