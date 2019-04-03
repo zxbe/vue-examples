a0bcfc2194de4e7b527f76be104f2dbe114a7647
Rick Beerendonk <rick@beerendonk.com>
2019-04-03 10:51:04 -0500

    Add "Vuex - Mutations - With Payload - Multiple Values" example.

---
+++ Vuex - 02 - Mutations - a. With Payload - ii. Multiple Values/runtime/main.js
@@ -0,0 +1,29 @@
+/*! European Union Public License version 1.2 !*/
+/*! Copyright © 2019 Rick Beerendonk          !*/
+
+import Vue from '../../node_modules/vue/dist/vue.esm.browser.js';
+import Vuex from '../../node_modules/vuex/dist/vuex.esm.js';
+
+import { addToList } from './helpers.js';
+
+// Always call this first.
+Vue.use(Vuex);
+
+const store = new Vuex.Store({
+  state: {
+    name: 'CreateStore'
+  },
+  mutations: {
+    changeName(state, payload) {
+      state.name = payload.name;
+    }
+  }
+});
+
+addToList(store.state);
+
+store.commit('changeName', {
+  name: 'ChangedName' /* potentially more values */
+});
+
+addToList(store.state);
