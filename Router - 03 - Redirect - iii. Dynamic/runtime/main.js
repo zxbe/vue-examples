7d583736d53fcbbe4bb7feb8cd31bdc98dbe49e3
Rick Beerendonk <rick@beerendonk.com>
2019-04-04 12:06:26 -0500

    Add "Router - Redirect - Dynamic" example.

---
+++ Router - 03 - Redirect - iii. Dynamic/runtime/main.js
@@ -0,0 +1,26 @@
+/*! European Union Public License version 1.2 !*/
+/*! Copyright © 2019 Rick Beerendonk          !*/
+
+import Vue from '../../node_modules/vue/dist/vue.esm.browser.js';
+import VueRouter from '../../node_modules/vue-router/dist/vue-router.esm.js';
+
+Vue.use(VueRouter);
+
+const Home = { template: '<h2>Home</h2>' };
+const Page1 = { template: '<h2>Page 1</h2>' };
+const Page2 = { template: '<h2>Page 2</h2>' };
+
+new Vue({
+  router: new VueRouter({
+    routes: [
+      { path: '/', component: Home },
+      { path: '/page1', component: Page1 },
+      { path: '/page2', component: Page2 },
+      {
+        path: '/oldpage2',
+        redirect: () =>
+          new Date().getMinutes() % 2 === 0 ? '/page2' : '/page1'
+      }
+    ]
+  })
+}).$mount('#app');
