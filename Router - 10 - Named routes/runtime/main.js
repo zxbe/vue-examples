f10278d8cbe0ca0478394986102af207b4c5ec28
Rick Beerendonk <rick@beerendonk.com>
2019-04-04 11:54:02 -0500

    Add "Router - Named routes" example.

---
+++ Router - 10 - Named routes/runtime/main.js
@@ -0,0 +1,21 @@
+/*! European Union Public License version 1.2 !*/
+/*! Copyright © 2019 Rick Beerendonk          !*/
+
+import Vue from '../../node_modules/vue/dist/vue.esm.browser.js';
+import VueRouter from '../../node_modules/vue-router/dist/vue-router.esm.js';
+
+Vue.use(VueRouter);
+
+const Home = { template: '<h2>Home</h2>' };
+const Page1 = { template: '<h2>Page 1</h2>' };
+const Page2 = { template: '<h2>Page 2</h2>' };
+
+new Vue({
+  router: new VueRouter({
+    routes: [
+      { path: '/', name: 'index', component: Home },
+      { path: '/page1', name: 'first', component: Page1 },
+      { path: '/page2', name: 'second', component: Page2 }
+    ]
+  })
+}).$mount('#app');
