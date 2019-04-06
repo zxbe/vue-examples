d9ec4db695d04dab2b523075cce530cd10220600
Rick Beerendonk <rick@beerendonk.com>
2019-04-05 19:00:01 -0500

    Add "Data Binding - Style - In Code" example.

---
+++ 02 - Data Binding - b. Style - ii. In Code/runtime/main.js
@@ -0,0 +1,15 @@
+/*! European Union Public License version 1.2 !*/
+/*! Copyright © 2019 Rick Beerendonk          !*/
+
+import Vue from '../../node_modules/vue/dist/vue.esm.browser.js';
+
+new Vue({
+  el: '#app',
+  data: {
+    style: {
+      fontSize: '48px',
+      fontStyle: 'italic',
+      textDecoration: 'underline'
+    }
+  }
+});
