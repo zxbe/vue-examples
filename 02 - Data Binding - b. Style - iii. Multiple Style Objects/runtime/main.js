a400e041beef15572e3d20e820ba5492b2e84b18
Rick Beerendonk <rick@beerendonk.com>
2019-04-05 19:12:44 -0500

    Add "Data Binding - Style - Multiple Style Objects" example.

---
+++ 02 - Data Binding - b. Style - iii. Multiple Style Objects/runtime/main.js
@@ -0,0 +1,18 @@
+/*! European Union Public License version 1.2 !*/
+/*! Copyright © 2019 Rick Beerendonk          !*/
+
+import Vue from '../../node_modules/vue/dist/vue.esm.browser.js';
+
+new Vue({
+  el: '#app',
+  data: {
+    style1: {
+      color: 'blue',
+      fontStyle: 'italic'
+    },
+    style2: {
+      color: 'red',
+      textDecoration: 'underline'
+    }
+  }
+});
