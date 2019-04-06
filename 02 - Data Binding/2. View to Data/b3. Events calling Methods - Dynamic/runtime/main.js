a3dc310481d8dd93bbfbe0e2e7239f84d534bbd8
Rick Beerendonk <rick@beerendonk.com>
2019-04-06 16:39:30 -0500

    Add "Data Binding - View to Data - Events calling Methods - Dynamic" example.

---
+++ 02 - Data Binding/2. View to Data/b3. Events calling Methods - Dynamic/runtime/main.js
@@ -0,0 +1,17 @@
+/*! European Union Public License version 1.2 !*/
+/*! Copyright © 2019 Rick Beerendonk          !*/
+
+import Vue from '../../../../node_modules/vue/dist/vue.esm.browser.js';
+
+new Vue({
+  el: '#app',
+  data: {
+    dynamicevent: 'click'
+  },
+  methods: {
+    clicked: function(e) {
+      alert(`Event handled.
+      Target: ${e.target}`);
+    }
+  }
+});
