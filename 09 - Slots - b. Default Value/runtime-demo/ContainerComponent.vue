f92d507c9026f4e3abf66750b8a09cf19c489ed5
Rick Beerendonk <rick@beerendonk.com>
2019-04-07 12:25:13 -0500

    Add "Slots - Default Value" example.

---
+++ 09 - Slots - b. Default Value/runtime-demo/ContainerComponent.vue
@@ -0,0 +1,23 @@
+<!-- European Union Public License version 1.2 -->
+<!-- Copyright © 2019 Rick Beerendonk          -->
+
+<template>
+  <div style="background-color: lightgray; padding: 10px">
+    The only item:
+    <div style="background-color: white; padding: 10px">
+      <slot>Default Slot Value</slot>
+    </div>
+  </div>
+</template>
+
+<script>
+/* global module */
+/* eslint vue/require-prop-types: "off" */
+
+// @vue/component
+module.exports = {
+  name: 'ContainerComponent'
+};
+</script>
+
+<style scoped></style>
