c6b87730543f7cdd56525ec5ae42c66a7c8981ab
Rick Beerendonk <rick@beerendonk.com>
2019-04-07 09:59:27 -0500

    Add "Slots - One" example.

---
+++ 09 - Slots - a. One/runtime/main.js
@@ -0,0 +1,14 @@
+/*! European Union Public License version 1.2 !*/
+/*! Copyright © 2019 Rick Beerendonk          !*/
+
+import Vue from '../../../node_modules/vue/dist/vue.esm.browser.js';
+
+import ContainerComponent from './ContainerComponent.js';
+
+// Bootstrap
+new Vue({
+  el: '#app',
+  components: {
+    'container-component': ContainerComponent
+  }
+});
