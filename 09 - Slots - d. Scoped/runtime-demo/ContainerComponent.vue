26a0f25f76eca158a7c24bb0b71e86a61376e881
Rick Beerendonk <rick@beerendonk.com>
2019-04-09 08:48:33 -0500

    Add "Slots - Scoped" examples.

---
+++ 09 - Slots - d. Scoped/runtime-demo/ContainerComponent.vue
@@ -0,0 +1,30 @@
+<!-- European Union Public License version 1.2 -->
+<!-- Copyright © 2019 Rick Beerendonk          -->
+
+<template>
+  <div style="background-color: lightgray; padding: 10px">
+    Normal:
+    <div style="background-color: white; padding: 10px">
+      <slot name="first"></slot>
+    </div>
+    Scoped:
+    <div style="background-color: white; padding: 10px">
+      <slot name="second" :source="source"></slot>
+    </div>
+  </div>
+</template>
+
+<script>
+/* global module */
+/* eslint vue/require-prop-types: "off" */
+
+// @vue/component
+module.exports = {
+  name: 'ContainerComponent',
+  data: () => ({
+    source: 'Container'
+  })
+};
+</script>
+
+<style scoped></style>
