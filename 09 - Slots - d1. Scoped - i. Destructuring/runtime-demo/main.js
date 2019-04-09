a6042689d3601ff816224eeaddcdf03a3a43ebb8
Rick Beerendonk <rick@beerendonk.com>
2019-04-09 16:09:59 -0500

    Add "Slots - Scoped - Destructuring" examples.

---
+++ 09 - Slots - d1. Scoped - i. Destructuring/runtime-demo/main.js
@@ -0,0 +1,17 @@
+/*! European Union Public License version 1.2 !*/
+/*! Copyright © 2019 Rick Beerendonk          !*/
+
+/* global httpVueLoader, Vue */
+
+const ContainerComponent = httpVueLoader('./ContainerComponent.vue');
+
+// Bootstrap
+new Vue({
+  el: '#app',
+  data: () => ({
+    source: 'Root'
+  }),
+  components: {
+    'container-component': ContainerComponent
+  }
+});
