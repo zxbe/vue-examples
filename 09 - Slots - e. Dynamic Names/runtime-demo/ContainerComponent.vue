f042ad5d9ba7fc624a158caac9a5738006d513b5
Rick Beerendonk <rick@beerendonk.com>
2019-04-10 07:25:15 -0500

    Add "Slots - Dynamic Names" examples.

---
+++ 09 - Slots - e. Dynamic Names/runtime-demo/ContainerComponent.vue
@@ -0,0 +1,27 @@
+<!-- European Union Public License version 1.2 -->
+<!-- Copyright © 2019 Rick Beerendonk          -->
+
+<template>
+  <div style="background-color: lightgray; padding: 10px">
+    The 1st item:
+    <div style="background-color: white; padding: 10px">
+      <slot name="first"></slot>
+    </div>
+    The 2nd item:
+    <div style="background-color: white; padding: 10px">
+      <slot name="second"></slot>
+    </div>
+  </div>
+</template>
+
+<script>
+/* global module */
+/* eslint vue/require-prop-types: "off" */
+
+// @vue/component
+module.exports = {
+  name: 'ContainerComponent'
+};
+</script>
+
+<style scoped></style>
