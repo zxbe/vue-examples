8cd2ac0a549fd9bd0be8af6259b27bf38ea70486
Rick Beerendonk <rick@beerendonk.com>
2019-04-10 09:50:02 -0500

    Add "Filters - Chaining" example.

---
+++ 05 - Filters - c. Chaining/runtime/main.js
@@ -0,0 +1,32 @@
+/*! European Union Public License version 1.2 !*/
+/*! Copyright © 2019 Rick Beerendonk          !*/
+
+import Vue from '../../node_modules/vue/dist/vue.esm.browser.js';
+
+new Vue({
+  el: '#app',
+  filters: {
+    reverse: function(value) {
+      if (!value) {
+        return '';
+      }
+
+      return value
+        .split('')
+        .reverse()
+        .join('');
+    },
+    titlecase: function(value) {
+      if (!value) {
+        return '';
+      }
+
+      return value
+        .split(' ')
+        .map(([first, ...rest]) =>
+          [first.toUpperCase(), ...rest.join('').toLowerCase()].join('')
+        )
+        .join(' ');
+    }
+  }
+});
