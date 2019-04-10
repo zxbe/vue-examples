5b65e345370cf3a495d9cd81bd0a5ba247ec9690
Rick Beerendonk <rick@beerendonk.com>
2019-04-10 08:39:02 -0500

    Add "Filters - UpperCase" example.

---
+++ 05 - Filters - TODO/b. UpperCase/runtime/main.js
@@ -0,0 +1,17 @@
+/*! European Union Public License version 1.2 !*/
+/*! Copyright © 2019 Rick Beerendonk          !*/
+
+import Vue from '../../../../node_modules/vue/dist/vue.esm.browser.js';
+
+new Vue({
+  el: '#app',
+  filters: {
+    uppercase: function(value) {
+      if (!value) {
+        return '';
+      }
+
+      return value.toUpperCase();
+    }
+  }
+});
