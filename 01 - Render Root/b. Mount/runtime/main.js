54c4e1a5202de8653e088cd2a33b51b913c13f0b
Rick Beerendonk <rick@beerendonk.com>
2019-12-08 12:53:20 +0100

    Reorganise examples.

--- 01 - Render Root/b. Mount/runtime/main.js
+++ 01 - Render Root/b. Mount/runtime/main.js
@@ -1,11 +1,12 @@
 /*! European Union Public License version 1.2 !*/
 /*! Copyright © 2018 Rick Beerendonk          !*/
 
-import Vue from '../../node_modules/vue/dist/vue.esm.browser.js';
+import Vue from '../../../node_modules/vue/dist/vue.esm.browser.js';
 
 new Vue({
   el: '#app',
   data: {
     greeting: 'Hello World!'
-  }
-});
+  },
+  template: '<div>{{ greeting }}</div>'
+}).$mount('#app');
