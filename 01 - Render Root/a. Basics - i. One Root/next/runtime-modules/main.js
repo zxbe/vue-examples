ba7fa381dae3acfab88d94bee71f7ca2fc5062a2
Rick Beerendonk <rick@beerendonk.com>
2020-01-03 22:33:53 +0100

    Use yarn to parallel install Vue 2 and Vue 3 (with alias).

--- 01 - Render Root/a. Basics - i. One Root/next/runtime-modules/main.js
+++ 01 - Render Root/a. Basics - i. One Root/next/runtime-modules/main.js
@@ -1,7 +1,7 @@
 /*! European Union Public License version 1.2 !*/
 /*! Copyright © 2020 Rick Beerendonk          !*/
 
-import * as Vue from '../../../../node_modules/vue/dist/vue.esm.js';
+import * as Vue from '../../../../node_modules/vue3/dist/vue.esm.js';
 
 Vue.createApp().mount(
   {
