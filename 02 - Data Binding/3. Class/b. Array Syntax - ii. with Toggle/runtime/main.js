cd136cfb7977c55719ba7223456df9d99903b0db
Rick Beerendonk <rick@beerendonk.com>
2020-01-06 22:06:14 +0100

    Addd "Data Binding - Class - Array Syntax - with Toggle" example.

---
+++ 02 - Data Binding/3. Class/b. Array Syntax - ii. with Toggle/runtime/main.js
@@ -0,0 +1,18 @@
+/*! European Union Public License version 1.2 !*/
+/*! Copyright © 2020 Rick Beerendonk          !*/
+
+import Vue from '../../../../node_modules/vue/dist/vue.esm.browser.js';
+
+new Vue({
+  el: '#app',
+  data: {
+    isDynamic1: false,
+    isDynamic2: true
+  },
+  methods: {
+    clicked: function() {
+      this.isDynamic1 = !this.isDynamic1;
+      this.isDynamic2 = !this.isDynamic2;
+    }
+  }
+});
