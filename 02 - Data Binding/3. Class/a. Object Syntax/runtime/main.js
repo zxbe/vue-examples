5ee4fa9cff1e358c5a499e5544330bc1dab3dc69
Rick Beerendonk <rick@beerendonk.com>
2020-01-06 07:45:44 +0100

    Reorder.

--- 02 - Data Binding/3. Class/a. Object Syntax/runtime/main.js
+++ 02 - Data Binding/3. Class/a. Object Syntax/runtime/main.js
@@ -1,7 +1,7 @@
 /*! European Union Public License version 1.2 !*/
 /*! Copyright © 2019 Rick Beerendonk          !*/
 
-import Vue from '../../node_modules/vue/dist/vue.esm.browser.js';
+import Vue from '../../../../node_modules/vue/dist/vue.esm.browser.js';
 
 new Vue({
   el: '#app',
