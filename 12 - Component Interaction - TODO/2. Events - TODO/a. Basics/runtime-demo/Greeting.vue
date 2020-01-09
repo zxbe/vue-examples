b919e650032b937504bea7d97dac80bf18d699be
Rick Beerendonk <rick@beerendonk.com>
2020-01-09 22:10:12 +0100

    Add "Component Interaction - Events - Basics (runtime-demo)" example.

---
+++ 12 - Component Interaction - TODO/2. Events - TODO/a. Basics/runtime-demo/Greeting.vue
@@ -0,0 +1,23 @@
+<!-- European Union Public License version 1.2 -->
+<!-- Copyright © 2020 Rick Beerendonk          -->
+
+<template>
+  <input :value="name" @keyup="changed" />
+</template>
+
+<script>
+/* eslint vue/require-prop-types: "off" */
+
+// @vue/component
+module.exports = {
+  name: 'Greeting',
+  props: ['name'],
+  methods: {
+    changed(e) {
+      this.$emit('change', e.target.value);
+    }
+  }
+};
+</script>
+
+<style scoped></style>
