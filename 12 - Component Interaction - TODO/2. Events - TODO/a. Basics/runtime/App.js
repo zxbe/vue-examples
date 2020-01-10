c66d51fd4fa6dd5d4f4d2181b6c8ca060563fd3d
Rick Beerendonk <rick@beerendonk.com>
2020-01-10 07:31:49 +0100

    Add runtime example.

---
+++ 12 - Component Interaction - TODO/2. Events - TODO/a. Basics/runtime/App.js
@@ -0,0 +1,30 @@
+/*! European Union Public License version 1.2 !*/
+/*! Copyright © 2020 Rick Beerendonk          !*/
+
+import Greeting from './Greeting.js';
+
+/* eslint vue/require-prop-types: "off" */
+
+// @vue/component
+export default {
+  name: 'App',
+  components: {
+    greeting: Greeting
+  },
+  data: function() {
+    return {
+      greetingText: 'World'
+    };
+  },
+  methods: {
+    greetingChanged: function(value) {
+      this.greetingText = value;
+    }
+  },
+  template: `
+    <div>
+      <greeting :name="greetingText" @change="greetingChanged($event)"></greeting>
+      <h1>Hello {{ greetingText }}</h1>
+    </div>
+  `
+};
