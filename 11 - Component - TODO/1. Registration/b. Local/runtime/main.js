1c12563233877fba2a494d7ad39ab20da17dc412
Rick Beerendonk <rick@beerendonk.com>
2020-01-10 22:55:36 +0100

    Fix paths.

--- 11 - Component - TODO/1. Registration/b. Local/runtime/main.js
+++ 11 - Component - TODO/1. Registration/b. Local/runtime/main.js
@@ -1,7 +1,7 @@
 /*! European Union Public License version 1.2 !*/
 /*! Copyright © 2018 Rick Beerendonk          !*/
 
-import Vue from '../../../node_modules/vue/dist/vue.esm.browser.js';
+import Vue from '../../../../node_modules/vue/dist/vue.esm.browser.js';
 
 import GreetingComponent from './GreetingComponent.js';
 
