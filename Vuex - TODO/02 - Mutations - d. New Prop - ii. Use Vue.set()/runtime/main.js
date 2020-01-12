58de93436492f167eeaa4a9c3dcc95d710658002
Rick Beerendonk <rick@beerendonk.com>
2020-01-12 20:29:29 +0100

    Regroup.

---
+++ Vuex - TODO/02 - Mutations - d. New Prop - ii. Use Vue.set()/runtime/main.js
@@ -0,0 +1,30 @@
+/*! European Union Public License version 1.2 !*/
+/*! Copyright © 2019 Rick Beerendonk          !*/
+
+import Vue from '../../../node_modules/vue/dist/vue.esm.browser.js';
+import Vuex from '../../../node_modules/vuex/dist/vuex.esm.js';
+
+import { addToList } from './helpers.js';
+
+// Always call this first.
+Vue.use(Vuex);
+
+const SET_NEW_PROP = 'SET_NEW_PROP';
+
+const store = new Vuex.Store({
+  state: {
+    obj: {}
+  },
+  mutations: {
+    [SET_NEW_PROP](state, payload) {
+      // Use Vue.set() for Reactivity
+      Vue.set(state, 'newProp', payload);
+    }
+  }
+});
+
+addToList(store.state);
+
+store.commit(SET_NEW_PROP, 123);
+
+addToList(store.state);
---
+++ Vuex - TODO/02 - Mutations - e. No async/runtime/main.js
@@ -0,0 +1,36 @@
+/*! European Union Public License version 1.2 !*/
+/*! Copyright © 2019 Rick Beerendonk          !*/
+
+import Vue from '../../../node_modules/vue/dist/vue.esm.browser.js';
+import Vuex from '../../../node_modules/vuex/dist/vuex.esm.js';
+
+import { addToList } from './helpers.js';
+
+// Always call this first.
+Vue.use(Vuex);
+
+const NAME_TO_LOWER_CASE = 'NAME_TO_LOWER_CASE';
+
+const store = new Vuex.Store({
+  state: {
+    name: 'CreateStore'
+  },
+  mutations: {
+    [NAME_TO_LOWER_CASE](state) {
+      // Never use async functions, because it is hard to debug.
+      // This is wrong:
+      (async () => {
+        // And this is also wrong:
+        setTimeout(() => {
+          state.name = state.name.toLowerCase();
+        }, 1000);
+      })();
+    }
+  }
+});
+
+addToList(store.state);
+
+store.commit(NAME_TO_LOWER_CASE);
+
+addToList(store.state);
