130417b5a1bceee0ac3d5669ea1012fbab5b5413
Rick Beerendonk <rick@beerendonk.com>
2020-01-14 21:54:40 +0100

    Add "Event Modifiers - Capture" examples.

---
+++ 08 - Event Modifiers - TODO/3. Capture/b. With/runtime-module/main.js
@@ -0,0 +1,26 @@
+/*! European Union Public License version 1.2 !*/
+/*! Copyright © 2020 Rick Beerendonk          !*/
+
+import Vue from '../../../../node_modules/vue/dist/vue.esm.browser.js';
+
+new Vue({
+  el: '#app',
+  methods: {
+    parentCapturingClicked: function(event) {
+      console.log(
+        `${event.currentTarget.id} received event for ${event.target.id} in capturing fase.`
+      );
+    },
+    parentAtTargetClicked: function(event) {
+      console.log(`${event.target.id} received event in at target fase.`);
+    },
+    parentBubblingClicked: function(event) {
+      console.log(
+        `${event.currentTarget.id} received event for ${event.target.id} in bubbling fase.`
+      );
+    },
+    childClicked: function(event) {
+      console.log(`${event.target.id} received event.`);
+    }
+  }
+});
