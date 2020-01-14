200ab11b237a993d3f9012936827c597414067fe
Rick Beerendonk <rick@beerendonk.com>
2020-01-14 22:09:51 +0100

    Add "Key Modifiers - Space" example.

---
+++ 09 - Key Modifiers - TODO/5. Space/runtime-module/main.js
@@ -0,0 +1,16 @@
+/*! European Union Public License version 1.2 !*/
+/*! Copyright © 2020 Rick Beerendonk          !*/
+
+import Vue from '../../../../node_modules/vue/dist/vue.esm.browser.js';
+
+new Vue({
+  el: '#app',
+  data: {
+    result: ''
+  },
+  methods: {
+    keyup: function(event) {
+      this.result += event.key + ',';
+    }
+  }
+});
