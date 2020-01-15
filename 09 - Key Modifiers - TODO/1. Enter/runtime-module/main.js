1496b3978e45e54947e0e08e47c35de02f58e7c5
Rick Beerendonk <rick@beerendonk.com>
2020-01-15 10:55:31 +0100

    Also show simultaneous pressed keys.

--- 09 - Key Modifiers - TODO/1. Enter/runtime-module/main.js
+++ 09 - Key Modifiers - TODO/1. Enter/runtime-module/main.js
@@ -10,7 +10,9 @@ new Vue({
   },
   methods: {
     keyup: function(event) {
-      this.result.push(`key: ${event.key}, code: ${event.code}`);
+      this.result.push(
+        `key: ${event.key}, code: ${event.code} (ctrlKey: ${event.ctrlKey}, shiftKey  ${event.shiftKey}, altKey: ${event.altKey}, metaKey: ${event.metaKey})`
+      );
     }
   }
 });
