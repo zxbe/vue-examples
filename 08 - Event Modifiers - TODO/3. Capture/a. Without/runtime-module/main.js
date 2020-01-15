18ecdee376140f4a2237157a5770ed0e428dc3a7
Rick Beerendonk <rick@beerendonk.com>
2020-01-15 08:49:19 +0100

    Improved UI (in page instead of console).

--- 08 - Event Modifiers - TODO/3. Capture/a. Without/runtime-module/main.js
+++ 08 - Event Modifiers - TODO/3. Capture/a. Without/runtime-module/main.js
@@ -5,17 +5,20 @@ import Vue from '../../../../node_modules/vue/dist/vue.esm.browser.js';
 
 new Vue({
   el: '#app',
+  data: {
+    result: []
+  },
   methods: {
     parentAtTargetClicked: function(event) {
-      console.log(`${event.target.id} received event in at target fase.`);
+      this.result.push(`${event.target.id} received event in at target fase.`);
     },
     parentBubblingClicked: function(event) {
-      console.log(
+      this.result.push(
         `${event.currentTarget.id} received event for ${event.target.id} in bubbling fase.`
       );
     },
     childClicked: function(event) {
-      console.log(`${event.target.id} received event.`);
+      this.result.push(`${event.target.id} received event.`);
     }
   }
 });
