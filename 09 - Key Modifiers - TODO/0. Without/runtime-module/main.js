15da1cd4bd3a239788ed9ecbfff6dcba608e5f48
Rick Beerendonk <rick@beerendonk.com>
2020-01-15 10:54:49 +0100

    Add "Key Modifiers - Without" example.

---
+++ 09 - Key Modifiers - TODO/0. Without/runtime-module/main.js
@@ -0,0 +1,18 @@
+/*! European Union Public License version 1.2 !*/
+/*! Copyright © 2020 Rick Beerendonk          !*/
+
+import Vue from '../../../../node_modules/vue/dist/vue.esm.browser.js';
+
+new Vue({
+  el: '#app',
+  data: {
+    result: []
+  },
+  methods: {
+    keyup: function(event) {
+      this.result.push(
+        `key: ${event.key}, code: ${event.code} (ctrlKey: ${event.ctrlKey}, shiftKey  ${event.shiftKey}, altKey: ${event.altKey}, metaKey: ${event.metaKey})`
+      );
+    }
+  }
+});
