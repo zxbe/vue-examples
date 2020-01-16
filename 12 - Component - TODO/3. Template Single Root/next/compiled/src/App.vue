ac7a122948d5e002ad7467df1f1f029f43a2dd3c
Rick Beerendonk <rick@beerendonk.com>
2020-01-16 10:44:37 +0100

    Fix example (Fragment is implicit).

--- 12 - Component - TODO/3. Template Single Root/next/compiled/src/App.vue
+++ 12 - Component - TODO/3. Template Single Root/next/compiled/src/App.vue
@@ -1,13 +1,11 @@
 <!-- European Union Public License version 1.2 -->
-<!-- Copyright © 2019 Rick Beerendonk          -->
+<!-- Copyright © 2020 Rick Beerendonk          -->
 
 <template>
-  <!-- Surrounding div required! -->
-  <div>
-    <h1>Hello World!</h1>
-    What do you think about Vue?
-    <div>It looks amazing.</div>
-  </div>
+  <!-- Surrounding div not required due to Fragments! -->
+  <h1>Hello World!</h1>
+  What do you think about Vue?
+  <div>It looks amazing.</div>
 </template>
 
 <script>
