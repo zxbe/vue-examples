1e9743fc6205746c5c23779ce5c46465cefb3f6b
Rick Beerendonk <rick@beerendonk.com>
2020-01-18 22:33:30 +0100

    Add compiled version of example (doesn't work!?!?!).

---
+++ 17 - Async Components - TODO/a. Basics/current/compiled - TODO FIX/src/components/GreetingDutch.vue
@@ -0,0 +1,22 @@
+<!-- European Union Public License version 1.2 -->
+<!-- Copyright © 2020 Rick Beerendonk          -->
+
+<template>
+  <div>
+    <label>Dutch: <input v-model="greeting"/></label>
+    <p>Hallo {{ greeting }}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'GreetingDutch',
+  data() {
+    return {
+      greeting: 'Wereld'
+    };
+  }
+};
+</script>
+
+<style scoped></style>
