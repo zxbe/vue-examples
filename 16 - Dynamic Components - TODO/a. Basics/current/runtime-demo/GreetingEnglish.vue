813c163c8d60eb6cd5db08142639977ddcef10fd
Rick Beerendonk <rick@beerendonk.com>
2020-01-18 21:42:30 +0100

    Add "Dynamic Components - Basics" demo.

---
+++ 16 - Dynamic Components - TODO/a. Basics/current/runtime-demo/GreetingEnglish.vue
@@ -0,0 +1,23 @@
+<!-- European Union Public License version 1.2 -->
+<!-- Copyright © 2020 Rick Beerendonk          -->
+
+<template>
+  <div>
+    <label>English: <input v-model="greeting"/></label>
+    <p>Hello {{ greeting }}</p>
+  </div>
+</template>
+
+<script>
+// @vue/component
+module.exports = {
+  name: 'GreetingEnglish',
+  data() {
+    return {
+      greeting: 'World'
+    };
+  }
+};
+</script>
+
+<style scoped></style>
---
+++ 16 - Dynamic Components - TODO/a. Basics/current/runtime-demo/GreetingSpanish.vue
@@ -0,0 +1,23 @@
+<!-- European Union Public License version 1.2 -->
+<!-- Copyright © 2020 Rick Beerendonk          -->
+
+<template>
+  <div>
+    <label>Spanish: <input v-model="greeting"/></label>
+    <p>Hola {{ greeting }}</p>
+  </div>
+</template>
+
+<script>
+// @vue/component
+module.exports = {
+  name: 'GreetingSpanish',
+  data() {
+    return {
+      greeting: 'Mundo'
+    };
+  }
+};
+</script>
+
+<style scoped></style>
