ef0c4060930396bb121df1ed35253b94b00eb75e
Rick Beerendonk <rick@beerendonk.com>
2020-01-18 22:27:06 +0100

    Add compiled version of example.

---
+++ 16 - Dynamic Components/b. Keep Alive/current/compiled/src/App.vue
@@ -0,0 +1,58 @@
+<!-- European Union Public License version 1.2 -->
+<!-- Copyright © 2019 Rick Beerendonk          -->
+
+<template>
+  <div>
+    <label>
+      <input v-model="language" type="radio" value="dutch" />
+      Dutch
+    </label>
+    <label>
+      <input v-model="language" type="radio" value="english" />
+      English
+    </label>
+    <label>
+      <input v-model="language" type="radio" value="spanish" />
+      Spanish
+    </label>
+
+    <div id="result">
+      <!-- Cache inactive components -->
+      <keep-alive>
+        <component :is="greetingLanguage"></component>
+      </keep-alive>
+    </div>
+  </div>
+</template>
+
+<script>
+import GreetingDutch from './components/GreetingDutch.vue';
+import GreetingEnglish from './components/GreetingEnglish.vue';
+import GreetingSpanish from './components/GreetingSpanish.vue';
+
+export default {
+  name: 'GreetingComponent',
+  components: {
+    'greeting-dutch': GreetingDutch,
+    'greeting-english': GreetingEnglish,
+    'greeting-spanish': GreetingSpanish
+  },
+  data() {
+    return {
+      language: 'english'
+    };
+  },
+  computed: {
+    greetingLanguage() {
+      return 'greeting-' + this.language;
+    }
+  }
+};
+</script>
+
+<style>
+#result {
+  padding: 4px;
+  background: lightgray;
+}
+</style>
