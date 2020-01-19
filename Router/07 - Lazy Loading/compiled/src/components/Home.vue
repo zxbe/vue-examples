d41d60898d1874ebb025e24ba73b421e19b9416c
Rick Beerendonk <rick@beerendonk.com>
2020-01-19 09:26:31 +0100

    Add compiled version.

---
+++ Router/07 - Lazy Loading/compiled/src/components/Home.vue
@@ -0,0 +1,14 @@
+<!-- European Union Public License version 1.2 -->
+<!-- Copyright © 2020 Rick Beerendonk          -->
+
+<template>
+  <h2>Home</h2>
+</template>
+
+<script>
+export default {
+  name: 'Home'
+};
+</script>
+
+<style scoped></style>
