383c1387e2daa021124cbb97576acc9148ea751d
Rick Beerendonk <rick@beerendonk.com>
2020-01-19 11:18:40 +0100

    Add "Router - Dynamic Routing - Multiple Segments" example.

---
+++ Router/04 - Dynamic Routing/b. Multiple Segments/runtime-module/main.js
@@ -0,0 +1,22 @@
+/*! European Union Public License version 1.2 !*/
+/*! Copyright © 2020 Rick Beerendonk          !*/
+
+import Vue from '../../../../node_modules/vue/dist/vue.esm.browser.js';
+import VueRouter from '../../../../node_modules/vue-router/dist/vue-router.esm.js';
+
+Vue.use(VueRouter);
+
+const Home = { template: '<h2>Home</h2>' };
+
+const Page = {
+  template: '<h2>Page {{ $route.params.id }}{{ $route.params.sub}} </h2>'
+};
+
+new Vue({
+  router: new VueRouter({
+    routes: [
+      { path: '/', component: Home },
+      { path: '/page/:id/:sub', component: Page }
+    ]
+  })
+}).$mount('#app');
