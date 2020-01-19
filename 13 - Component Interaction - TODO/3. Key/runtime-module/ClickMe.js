a8c79605dfab6b81079abd705e5f4415ff3b1afc
Rick Beerendonk <rick@beerendonk.com>
2020-01-19 23:21:11 +0100

    Add "Component Interaction - Key" example.

---
+++ 13 - Component Interaction - TODO/3. Key/runtime-module/ClickMe.js
@@ -0,0 +1,13 @@
+/*! European Union Public License version 1.2 !*/
+/*! Copyright © 2020 Rick Beerendonk          !*/
+
+export default {
+  name: 'ClickMe',
+  data() {
+    return {
+      count: 0
+    };
+  },
+  template:
+    '<a @click.prevent="count++">This link has been clicked {{ count }} times.</a>'
+};
