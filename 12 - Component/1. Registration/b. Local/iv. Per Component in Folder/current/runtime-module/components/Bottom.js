6922b109bf6cff4ac6830fcf57abcc3122d556d1
Rick Beerendonk <rick@beerendonk.com>
2020-01-20 17:13:26 +0100

    Add "Component - Registration - Local - Per Component in Folder" example.

---
+++ 12 - Component/1. Registration/b. Local/iv. Per Component in Folder/current/runtime-module/components/Bottom.js
@@ -0,0 +1,7 @@
+/*! European Union Public License version 1.2 !*/
+/*! Copyright © 2020 Rick Beerendonk          !*/
+
+export default {
+  name: 'Bottom',
+  template: '<h3>Bottom</h3>'
+};
---
+++ 12 - Component/1. Registration/b. Local/iv. Per Component in Folder/current/runtime-module/components/Top.js
@@ -0,0 +1,17 @@
+/*! European Union Public License version 1.2 !*/
+/*! Copyright © 2020 Rick Beerendonk          !*/
+
+import Middle from './Middle.js';
+
+export default {
+  name: 'Top',
+  components: {
+    Middle
+  },
+  template: `
+    <div>
+      <h1>Top</h1>
+      <Middle />
+    </div>
+  `
+};
