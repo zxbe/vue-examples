76a5e6fe29fc78ff393a93a5e96f179a13a20990
Rick Beerendonk <rick@beerendonk.com>
2020-01-20 07:14:37 +0100

    Add "Component - Style - Scoped" example.

---
+++ 12 - Component - TODO/5. Style/b. Scoped/compiled/src/App.vue
@@ -0,0 +1,25 @@
+<!-- European Union Public License version 1.2 -->
+<!-- Copyright © 2019 Rick Beerendonk          -->
+
+<template>
+  <div id="app">
+    <Top />
+  </div>
+</template>
+
+<script>
+import Top from './components/Top.vue';
+
+export default {
+  name: 'App',
+  components: {
+    Top
+  }
+};
+</script>
+
+<style>
+#app {
+  color: #444;
+}
+</style>
---
+++ 12 - Component - TODO/5. Style/b. Scoped/compiled/src/components/Middle.vue
@@ -0,0 +1,27 @@
+<!-- European Union Public License version 1.2 -->
+<!-- Copyright © 2020 Rick Beerendonk          -->
+
+<template>
+  <div id="middle">
+    <h2>Middle</h2>
+    <Bottom />
+  </div>
+</template>
+
+<script>
+import Bottom from './Bottom.vue';
+
+export default {
+  name: 'Middle',
+  components: {
+    Bottom
+  }
+};
+</script>
+
+<style scoped>
+#middle {
+  background-color: #d676ff;
+  color: #444;
+}
+</style>
