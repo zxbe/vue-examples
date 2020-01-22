4e85dd6f5bcd3f4d7c9c7af1ee3806d21fcc3ecc
Rick Beerendonk <rick@beerendonk.com>
2020-01-22 15:01:09 +0100

    Fix paths.

--- 08 - Component/3. Template Single Root/current/runtime-demo/main.js
+++ 08 - Component/3. Template Single Root/current/runtime-demo/main.js
@@ -1,7 +1,7 @@
 /*! European Union Public License version 1.2 !*/
 /*! Copyright © 2020 Rick Beerendonk          !*/
 
-import Vue from '../../../node_modules/vue/dist/vue.esm.browser.js';
+import Vue from '../../../../node_modules/vue/dist/vue.esm.browser.js';
 
 /* global httpVueLoader */
 const App = httpVueLoader('./App.vue');
