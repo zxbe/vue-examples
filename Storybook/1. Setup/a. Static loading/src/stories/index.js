21aeb73ded56ea69b20670292b922184e4dcf74e
Rick Beerendonk <rick@beerendonk.com>
2020-01-23 10:05:53 +0100

    Add one EditBox story (WIP).

--- Storybook/1. Setup/a. Static loading/src/stories/index.js
+++ Storybook/1. Setup/a. Static loading/src/stories/index.js
@@ -1,14 +1,30 @@
 import Vue from 'vue';
 import { storiesOf } from '@storybook/vue';
+//import { action } from '@storybook/addon-actions';
 import { text } from '@storybook/addon-knobs';
 
 // Import your custom components.
+import EditBox from '../components/EditBox.vue';
 import Greeting from '../components/Greeting.vue';
 import GreetingEditor from '../components/GreetingEditor.vue';
 
 // Register custom components.
+Vue.component('EditBox', EditBox);
 Vue.component('Greeting', Greeting);
 Vue.component('GreetingEditor', GreetingEditor);
+
+storiesOf('EditBox', module).add('with value', () => ({
+  components: {
+    EditBox
+  },
+  props: {
+    value: 'Test'
+  },
+  template: '<EditBox @change="() => {}" value="Test" />'
+}));
+//  .add('with onChange', () => (
+//    <EditBox v-on:change={action('editbox-change')} />
+//  ));
 
 storiesOf('Greeting', module)
   .add('without name', () => '<Greeting/>', {
--- Storybook/1. Setup/b. Dynamic loading/src/components/EditBox.stories.js
+++ Storybook/1. Setup/b. Dynamic loading/src/components/EditBox.stories.js
@@ -1,5 +1,5 @@
 import Vue from 'vue';
-//import { storiesOf } from '@storybook/vue';
+import { storiesOf } from '@storybook/vue';
 //import { action } from '@storybook/addon-actions';
 
 // Import your custom component.
@@ -8,8 +8,15 @@ import EditBox from './EditBox.vue';
 // Register custom component.
 Vue.component('EditBox', EditBox);
 
-//storiesOf('EditBox', module)
-//  .add('with value', () => <EditBox v-on:change="() => {}" value="Test" />)
+storiesOf('EditBox', module).add('with value', () => ({
+  components: {
+    EditBox
+  },
+  props: {
+    value: 'Test'
+  },
+  template: '<EditBox @change="() => {}" value="Test" />'
+}));
 //  .add('with onChange', () => (
 //    <EditBox v-on:change={action('editbox-change')} />
 //  ));
