3ef283557a4720ff41c835704a04443016f8f7ed
Rick Beerendonk <rick@beerendonk.com>
2020-01-24 13:43:06 +0100

    Add "Vuex - Actions - Destructure Context" example.

---
+++ Vuex - TODO/03 - Actions - a. Destructure context/runtime-module/main.js
@@ -0,0 +1,32 @@
+/*! European Union Public License version 1.2 !*/
+/*! Copyright © 2020 Rick Beerendonk          !*/
+
+import Vue from '../../../node_modules/vue/dist/vue.esm.browser.js';
+import Vuex from '../../../node_modules/vuex/dist/vuex.esm.js';
+
+import { addToList } from './helpers.js';
+
+// Always call this first.
+Vue.use(Vuex);
+
+const store = new Vuex.Store({
+  state: {
+    name: 'CreateStore'
+  },
+  mutations: {
+    nameToLowerCase(state) {
+      state.name = state.name.toLowerCase();
+    }
+  },
+  actions: {
+    nameToLowerCase({ commit }) {
+      commit('nameToLowerCase');
+    }
+  }
+});
+
+addToList(store.state);
+
+store.dispatch('nameToLowerCase');
+
+addToList(store.state);
