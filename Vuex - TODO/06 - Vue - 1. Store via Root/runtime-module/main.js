37c35e74af0115763098755e9c8d500875649436
Rick Beerendonk <rick@beerendonk.com>
2020-01-30 18:10:36 +0100

    Add "Vuex - Vue - Store via Root" example.

---
+++ Vuex - TODO/06 - Vue - 1. Store via Root/runtime-module/main.js
@@ -0,0 +1,25 @@
+/*! European Union Public License version 1.2 !*/
+/*! Copyright © 2020 Rick Beerendonk          !*/
+
+import Vue from '../../../node_modules/vue/dist/vue.esm.browser.js';
+import Vuex from '../../../node_modules/vuex/dist/vuex.esm.browser.js';
+
+Vue.use(Vuex);
+
+const store = new Vuex.Store();
+
+const App = {
+  name: 'App',
+  computed: {
+    state() {
+      return this.$store.state;
+    }
+  },
+  template: '<h1>State: {{ state }}</h1>'
+};
+
+new Vue({
+  el: '#app',
+  store,
+  render: h => h(App)
+});
