0c9777c7c14f3b3e70026e72517252595622ca8b
Rick Beerendonk <rick@beerendonk.com>
2020-01-30 22:20:36 +0100

    Simplification.

--- Vuex/03 - Mutations - b. With Payload - ii. Multiple Values/runtime-module/main.js
+++ Vuex/03 - Mutations - b. With Payload - ii. Multiple Values/runtime-module/main.js
@@ -9,23 +9,26 @@ import { addToList } from './helpers.js';
 // Always call this first.
 Vue.use(Vuex);
 
-const CHANGE_NAME = 'CHANGE_NAME';
+const CHANGE = 'change';
 
 const store = new Vuex.Store({
   state: {
-    name: 'CreateStore'
+    name: 'Initial',
+    year: 2019
   },
   mutations: {
-    [CHANGE_NAME](state, payload) {
+    [CHANGE](state, payload) {
       state.name = payload.name;
+      state.year = payload.year;
     }
   }
 });
 
 addToList(store.state);
 
-store.commit(CHANGE_NAME, {
-  name: 'ChangedName' /* potentially more values */
+store.commit(CHANGE, {
+  name: 'Update',
+  year: 2020
 });
 
 addToList(store.state);
